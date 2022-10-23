3b88239c9e6e75f2de137408784163972125cc5b
Eden <edenyaron5@gmail.com>
2022-10-23 12:25:12 +0300

    screen

--- Screen.py
+++ Screen.py
@@ -1,2 +1,7 @@
 import pygame
-pygame.display.set_mode((750*2,750))
+import consts
+screen = pygame.display.set_mode((consts.SCREEN_WIDTH, consts.SCREEN_HEIGHT))
+pygame.display.set_caption("The Flag")
+screen.fill(consts.BACKGROUND_COLOR)
+
+pygame.quit()
