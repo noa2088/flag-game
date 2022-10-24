4433e928d872b665fd605521d63a606f13acb171
Eden <edenyaron5@gmail.com>
2022-10-24 12:33:39 +0300

    main

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ state = {"is window open": True, "soldier location": consts.INITIAL_SOLDIER_POSI
 
 def main():
     pygame.init()
-    Screen.draw_game()
+    Screen.draw_game(state["soldier location"])
     while state["is window open"]:
         events = pygame.event.get()
         for event in events:
@@ -20,9 +20,10 @@ def main():
                     state["direction"] = consts.UP
                 elif event.key == pygame.K_DOWN:
                     state["direction"] = consts.DOWN
-                elif event.key == pygame.K_KP_ENTER:
+                elif event.key == pygame.K_KP_ENTER or pygame.K_RETURN:
                     Screen.night_screen()
                     pygame.time.wait(1000)
+                    Screen.draw_game(state["soldier location"])
                 move_soldier(state["direction"])
                 Screen.draw_soldier(state["soldier location"])
             elif event.type == pygame.QUIT:
@@ -31,17 +32,27 @@ def main():
 
 def move_soldier(direction):
     if direction == consts.UP:
-        if state["soldier location"][1] > 0:
-            state["soldier location"][1] = state["soldier location"][1] - consts.STEP
+        if state["soldier location"][1] >= consts.SQUARE_HEIGHT:
+            position = list(state["soldier location"])
+            position[1] -= consts.STEP
+            state["soldier location"] = tuple(position)
     elif direction == consts.DOWN:
-        if state["soldier location"][1] < consts.SCREEN_ROWS - 1:
-            state["soldier location"][1] = state["soldier location"][1] + consts.STEP
+        if state["soldier location"][1] < consts.SCREEN_HEIGHT - consts.SOLDIER_HEIGHT * consts.SQUARE_HEIGHT - \
+                consts.SQUARE_HEIGHT:
+            position = list(state["soldier location"])
+            position[1] += consts.STEP
+            state["soldier location"] = tuple(position)
     elif direction == consts.RIGHT:
-        if state["soldier location"][0] < consts.SCREEN_COLUMNS - 1:
-            state["soldier location"][0] = state["soldier location"][0] + consts.STEP
+        if state["soldier location"][0] < consts.SCREEN_WIDTH - consts.SOLDIER_WIDTH * consts.SQUARE_WIDTH - \
+                consts.SQUARE_WIDTH:
+            position = list(state["soldier location"])
+            position[1] += consts.STEP
+            state["soldier location"] = tuple(position)
     elif direction == consts.LEFT:
-        if state["soldier location"][0] > 0:
-            state["soldier location"][0] = state["soldier location"][0] - consts.STEP
+        if state["soldier location"][0] >= consts.SQUARE_WIDTH:
+            position = list(state["soldier location"])
+            position[1] -= consts.STEP
+            state["soldier location"] = tuple(position)
     return state["soldier location"]
 
 
