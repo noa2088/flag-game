886305751e89d8918c146115c557b55a41d05a6a
Eden <edenyaron5@gmail.com>
2022-10-24 11:46:31 +0300

    main

--- main.py
+++ main.py
@@ -1,26 +1,52 @@
 import pygame
+import Screen
+import consts
 
-state = {"is window open": True}
+state = {"is window open": True, "soldier location": consts.INITIAL_SOLDIER_POSITION, "direction": None}
 
 
 def main():
     pygame.init()
+    Screen.draw_game()
     while state["is window open"]:
         events = pygame.event.get()
         for event in events:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    pass
+                    state["direction"] = consts.LEFT
                 elif event.key == pygame.K_RIGHT:
-                    pass
+                    state["direction"] = consts.RIGHT
                 elif event.key == pygame.K_UP:
-                    pass
+                    state["direction"] = consts.UP
                 elif event.key == pygame.K_DOWN:
-                    pass
+                    state["direction"] = consts.DOWN
                 elif event.key == pygame.K_KP_ENTER:
-                    pass
+                    Screen.night_screen()
+                    pygame.time.wait(1000)
+                move_soldier(state["direction"])
+                Screen.draw_soldier(state["soldier location"])
             elif event.type == pygame.QUIT:
                 state["is window open"] = False
+
+
+def move_soldier(direction):
+    if direction == consts.UP:
+        if state["soldier location"][1] > 0:
+            state["soldier location"][1] = state["soldier location"][1] - consts.STEP
+    elif direction == consts.DOWN:
+        if state["soldier location"][1] < consts.SCREEN_ROWS - 1:
+            state["soldier location"][1] = state["soldier location"][1] + consts.STEP
+    elif direction == consts.RIGHT:
+        if state["soldier location"][0] < consts.SCREEN_COLUMNS - 1:
+            state["soldier location"][0] = state["soldier location"][0] + consts.STEP
+    elif direction == consts.LEFT:
+        if state["soldier location"][0] > 0:
+            state["soldier location"][0] = state["soldier location"][0] - consts.STEP
+    return state["soldier location"]
+
+
+def current_soldier_position():
+    pass
 
 
 def did_soldier_reach_the_flag():
@@ -29,3 +55,7 @@ def did_soldier_reach_the_flag():
 
 def did_soldier_reach_a_mine():
     pass
+
+
+
+main()
